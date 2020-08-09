6619bf26d43a1209de140ea77a46d43f696bd536
ishajain <write2ishajain@gmail.com>
2020-08-08 23:05:38 -0400

    Using tutorial to help me learn. Ended off at 15:01

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
+const app = express();
+
+//EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// Routes
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log('Server started on port ${PORT}'));
