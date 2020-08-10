2b5a0b03c98c2b71e95d97b69df620608e7eef35
ishajain <write2ishajain@gmail.com>
2020-08-10 19:44:14 -0400

    Made it up to 1 hour in the tutorial

--- app.js
+++ app.js
@@ -1,13 +1,46 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express();
+
+//DB Config
+const db = require('./config/keys').MongoURI;
+
+//Connect to Mongo
+mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
 
 //EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
-// Routes
+//Bodyparser
+app.use(express.urlencoded({extended: false}));
+
+//Express Session 
+app.use(
+    session({
+      secret: 'secret',
+      resave: true,
+      saveUninitialized: true
+    })
+);
+
+//Connect flash
+app.use(flash());
+
+//Global Vars
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    next();
+});
+
+//Routes
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://ijain:pass0915@cluster0.1cls1.mongodb.net/test?retryWrites=true&w=majority'
+}
